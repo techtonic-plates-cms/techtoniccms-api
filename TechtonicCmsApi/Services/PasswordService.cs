78d4d49101c10170f6339675644a8ab23a4c0334
co <cockernutx@gmail.com>
2026-04-30 00:44:21 +0000

    Replaced Bcrypt with Argon2

--- TechtonicCmsApi/Services/PasswordService.cs
+++ TechtonicCmsApi/Services/PasswordService.cs
@@ -1,20 +1,47 @@
 using System.Security.Cryptography;
 using System.Text;
+using Isopoh.Cryptography.Argon2;
 
 namespace TechtonicCmsApi.Services;
 
 public class PasswordService
 {
+    private static readonly Argon2Config DefaultConfig = new()
+    {
+        Type = Argon2Type.DataIndependentAddressing,
+        Version = Argon2Version.Nineteen,
+        TimeCost = 3,
+        MemoryCost = 65536,
+        Lanes = 4,
+        Threads = 4,
+        HashLength = 32,
+    };
+
     public string HashPassword(string password)
     {
-        return BCrypt.Net.BCrypt.HashPassword(password, 12);
+        var config = new Argon2Config
+        {
+            Type = DefaultConfig.Type,
+            Version = DefaultConfig.Version,
+            TimeCost = DefaultConfig.TimeCost,
+            MemoryCost = DefaultConfig.MemoryCost,
+            Lanes = DefaultConfig.Lanes,
+            Threads = DefaultConfig.Threads,
+            HashLength = DefaultConfig.HashLength,
+            Password = Encoding.UTF8.GetBytes(password),
+            Salt = RandomNumberGenerator.GetBytes(16),
+        };
+
+        var argon2 = new Argon2(config);
+        using var hash = argon2.Hash();
+        return config.EncodeString(hash.Buffer);
     }
 
     public (bool isValid, string? newHash) VerifyPassword(string password, string existingHash)
     {
-        if (existingHash.StartsWith("$2"))
+        if (existingHash.StartsWith("$argon2"))
         {
-            bool isValid = BCrypt.Net.BCrypt.Verify(password, existingHash);
+            bool isValid = Argon2.Verify(existingHash, password);
             return (isValid, null);
         }
 
